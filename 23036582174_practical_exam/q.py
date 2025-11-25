4a4812d75a9ffb253dbd0dc652300f49a789aa8d
kimsan <kimsan.em11@gmail.com>
2025-11-25 16:33:03 +0530

    practical exam

--- 23036582174_practical_exam/q.py
+++ 23036582174_practical_exam/q.py
@@ -20,30 +20,30 @@ y = df['loan_approved']
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 # K-NN Model
-# k = 5
-# knn = KNeighborsClassifier(n_neighbors=k)
-# knn.fit(X_train, y_train)
+k = 5
+knn = KNeighborsClassifier(n_neighbors=k)
+knn.fit(X_train, y_train)
 
-# knn_pred = knn.predict(X_test)
-# knn_pred_decode = le.inverse_transform(knn_pred)
-# y_test_decode = le.inverse_transform(y_test)
-# print(knn_pred_decode)
-# print(classification_report(y_test_decode, knn_pred_decode))
-# print("KNN 6-Fold Accuracy:")
-# print(cross_val_score(knn, x, y, cv=6))
+knn_pred = knn.predict(X_test)
+knn_pred_decode = le.inverse_transform(knn_pred)
+y_test_decode = le.inverse_transform(y_test)
+print(knn_pred_decode)
+print(classification_report(y_test_decode, knn_pred_decode))
+print("KNN 6-Fold Accuracy:")
+print(cross_val_score(knn, x, y, cv=6))
 
 
 # Decision Tree Model
-dt = DecisionTreeClassifier(
-    criterion='gini',       
-    max_depth=None                  
-)
+# dt = DecisionTreeClassifier(
+#     criterion='gini',       
+#     max_depth=None                  
+# )
 
-dt.fit(X_train, y_train)
+# dt.fit(X_train, y_train)
 
-dt_pred = dt.predict(X_test)
-dt_pred_decode = le.inverse_transform(dt_pred)
-print(dt_pred_decode)
-print(classification_report(y_test, dt_pred_decode))
-print("Decision Tree 6-Fold Accuracy:")
-print(cross_val_score(dt, x, y, cv=6))
+# dt_pred = dt.predict(X_test)
+# dt_pred_decode = le.inverse_transform(dt_pred)
+# print(dt_pred_decode)
+# print(classification_report(y_test, dt_pred_decode))
+# print("Decision Tree 6-Fold Accuracy:")
+# print(cross_val_score(dt, x, y, cv=6))
